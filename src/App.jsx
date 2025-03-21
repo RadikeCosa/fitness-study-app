cdd1f3c7e68fceda01383e01bf2dd776b771a9b8
Ramiro Cosa <ramirocosa@hotmail.com>
2025-03-21 14:20:09 -0300

    Add Argestan title and structure UI with basic CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import Header from "./components/Header";
 import HiitTimer from "./components/HiitTimer";
 import Pomodoro from "./components/Pomodoro";
 import Quotes from "./components/Quotes";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <HiitTimer />
-      <Pomodoro />
-      <Quotes />
+    <div className="app-container">
+      <header>
+        <Header />
+        <h1>Argestan</h1>
+      </header>
+      <main>
+        <section className="timers">
+          <HiitTimer />
+          <Pomodoro />
+        </section>
+        <section className="quotes">
+          <Quotes />
+        </section>
+      </main>
     </div>
   );
 }
