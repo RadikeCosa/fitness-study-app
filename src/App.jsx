c260336dcf4551fac05a6b4af3df535436a6b965
Ramiro Cosa <ramirocosa@hotmail.com>
2025-03-21 12:50:08 -0300

    Integrate HiitTimer into main App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import HiitTimer from "./components/HiitTimer";
 
 function App() {
   return (
     <div>
       <Header />
+      <HiitTimer />
     </div>
   );
 }
