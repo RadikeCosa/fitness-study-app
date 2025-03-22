8bd6817f2674331925f7d55bf1fce05098ae668d
Ramiro Cosa <ramirocosa@hotmail.com>
2025-03-22 11:27:01 -0300

    Actualizar Quotes para usar quotes.json

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,26 +1,8 @@
 import { useState } from "react";
 import "./Quotes.css";
+import quotes from "../data/quotes.json"; // Importamos el JSON
 
 function Quotes() {
-  const quotes = [
-    {
-      text: "El éxito no es la clave de la felicidad. La felicidad es la clave del éxito.",
-      author: "Albert Schweitzer",
-    },
-    {
-      text: "La disciplina es el puente entre metas y logros.",
-      author: "Jim Rohn",
-    },
-    {
-      text: "No te rindas, el comienzo es siempre lo más difícil.",
-      author: "Proverbio",
-    },
-    {
-      text: "El único lugar donde el éxito viene antes que el trabajo es en el diccionario.",
-      author: "Vidal Sassoon",
-    },
-  ];
-
   const [currentQuote, setCurrentQuote] = useState(0);
 
   const handleNextQuote = () => {
