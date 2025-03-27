dd3477107d4a912c4ec7546a904d693fbdcefb3a
Ramiro Cosa <ramirocosa@hotmail.com>
2025-03-27 10:20:10 -0300

    se integra la api de quotes con el componente para servir las citas

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,15 +1,35 @@
 // src/components/Quotes.jsx
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Quotes.css";
-import quotes from "../data/quotes.json";
 
 function Quotes() {
   const [currentQuote, setCurrentQuote] = useState(0);
+  const [quotes, setQuotes] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchQuotes() {
+      try {
+        const apiUrl = `${import.meta.env.VITE_API_URL}/api/quote/all`;
+        console.log("Fetching from:", apiUrl);
+        const response = await fetch(apiUrl);
+        if (!response.ok)
+          throw new Error(`Error al cargar las citas: ${response.status}`);
+        const data = await response.json();
+        setQuotes(data);
+        console.log("Datos Recibidos", data);
+      } catch (err) {
+        setError("Error", err);
+      }
+    }
+    fetchQuotes();
+  }, []);
 
   const handleNextQuote = () => {
     setCurrentQuote((prev) => (prev + 1) % quotes.length);
   };
-
+  if (error) return <p>{error}</p>;
+  if (quotes.length === 0) return <p>Cargando citas...</p>;
   return (
     <blockquote className="blockquote" onClick={handleNextQuote}>
       <p>{quotes[currentQuote].text}</p>
